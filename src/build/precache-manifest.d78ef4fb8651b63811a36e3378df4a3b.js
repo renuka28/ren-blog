c75edc489bcee64370a186b7cbe69fd3f9c2d477
renuka28 <mr.renukaprasad@gmail.com>
2020-07-09 11:42:38 -0500

    Initial Version

---
+++ src/build/precache-manifest.d78ef4fb8651b63811a36e3378df4a3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e937ddc0b59942007529",
+    "url": "/static/css/main.10a4e551.chunk.css"
+  },
+  {
+    "revision": "e937ddc0b59942007529",
+    "url": "/static/js/main.d1d4b8fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2e1fb1904fd6346f55cc",
+    "url": "/static/js/2.06e7c7cd.chunk.js"
+  },
+  {
+    "revision": "fd2599db1075f7acfebbb647abea7d5a",
+    "url": "/index.html"
+  }
+];
